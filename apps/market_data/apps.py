12f93023fed5f912c5c4b5b60b0511040108d6d2
Cptzeph23 <asekazephaniah@gmail.com>
2026-03-17 20:32:21 +0300

    market data apps

---
+++ apps/market_data/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+ 
+ 
+class MarketDataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.market_data'
+ 
+    def ready(self):
+        try:
+            import apps.market_data.signals  # noqa: F401
+        except ImportError:
+            pass
+ 
