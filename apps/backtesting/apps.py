8f38947bde8993b9cca0f60aa55e009c894e5f72
Cptzeph23 <asekazephaniah@gmail.com>
2026-03-19 20:41:32 +0300

    backtesting apps

--- apps/backtesting/apps.py
+++ apps/backtesting/apps.py
@@ -3,10 +3,12 @@ from django.apps import AppConfig
  
 class BacktestingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.backtesting'
+    name               = 'apps.backtesting'
+    verbose_name       = 'Backtesting'
  
     def ready(self):
         try:
             import apps.backtesting.signals  # noqa: F401
         except ImportError:
             pass
+ 
